49773ea0e66e0c5fb68dfc00b8dffd968765b46a
tu.dang <tu.dang@monstar-lab.com>
2023-10-19 16:03:39 +0900

    fix build error

--- ios/Classes/SwiftFlutterProxyPlugin.swift
+++ ios/Classes/SwiftFlutterProxyPlugin.swift
@@ -20,18 +20,17 @@ public class SwiftFlutterProxyPlugin: NSObject, FlutterPlugin {
   }
 
     func getProxySetting() -> NSDictionary? {
-    guard let proxySettings = CFNetworkCopySystemProxySettings()?.takeUnretainedValue(),
-        let url = URL(string: "https://www.bing.com/") else {
-            return nil
-    }
-    let proxies = CFNetworkCopyProxiesForURL((url as CFURL), proxySettings).takeUnretainedValue() as NSArray
-    guard let settings = proxies.firstObject as? NSDictionary,
-        let _ = settings.object(forKey: (kCFProxyTypeKey as String)) as? String else {
-            return nil
-    }
-    var proxyDictionary: NSDictionary?
-    for proxy in proxies {
-        if let proxyDictionary = proxy as? NSDictionary {
+        guard let proxySettings = CFNetworkCopySystemProxySettings()?.takeUnretainedValue(),
+            let url = URL(string: "https://www.bing.com/") else {
+                return nil
+        }
+        let proxies = CFNetworkCopyProxiesForURL((url as CFURL), proxySettings).takeUnretainedValue() as NSArray
+        guard let settings = proxies.firstObject as? NSDictionary,
+            let _ = settings.object(forKey: (kCFProxyTypeKey as String)) as? String else {
+                return nil
+        }
+        var proxyDictionary: NSDictionary?
+        for proxy in proxies {
             if let autoconfigUrl = proxyDictionary.value(forKey: (kCFProxyAutoConfigurationURLKey as String)), let proxyURL = URL(string: String(describing: autoconfigUrl)) as CFURL?, let hostCfUrl = url as CFURL? {
                 var context = CFStreamClientContext(version: CFIndex(0), info: nil, retain: nil, release: nil, copyDescription: nil)
                 let runLoopSource = CFNetworkExecuteProxyAutoConfigurationURL(proxyURL , hostCfUrl, {(_, proxies, __ ) in
@@ -51,15 +50,15 @@ public class SwiftFlutterProxyPlugin: NSObject, FlutterPlugin {
                 let runLoop: CFRunLoop = CFRunLoopGetCurrent()
                 CFRunLoopRemoveSource(CFRunLoopGetCurrent(), runLoopSource.takeUnretainedValue(), CFRunLoopMode.defaultMode)
             }
-        }
-    }
 
-    if let proxyDict = proxyDictionary {
-        return proxyDict
+        }
+
+        if let proxyDict = proxyDictionary {
+            return proxyDict
+        }
+        if let hostName = settings.object(forKey: (kCFProxyHostNameKey as String)), let port = settings.object(forKey: (kCFProxyPortNumberKey as String)) {
+            return ["host":hostName, "port":port] as NSDictionary
+        }
+        return nil
     }
-    if let hostName = settings.object(forKey: (kCFProxyHostNameKey as String)), let port = settings.object(forKey: (kCFProxyPortNumberKey as String)) {
-        return ["host":hostName, "port":port] as NSDictionary
-    }
-    return nil
-}
 }
